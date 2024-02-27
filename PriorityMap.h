f56c04e536b419228c19a79fe242a491540d795b
anthony-s-flath <123764940+anthony-s-flath@users.noreply.github.com>
2024-02-27 16:35:37 -0500

    added files

---
+++ PriorityMap.h
@@ -0,0 +1,276 @@
+/*
+
+    Container that iterates over Elements under one or more Keys.
+
+*/
+
+#ifndef PRIORITYMAP_H
+#define PRIORITYMAP_H
+
+#include <vector>
+#include <unordered_map>
+
+using std::vector, std::unordered_map;
+
+template <typename Key, typename Element>
+class PriorityMap {
+private:
+    const Key key;
+    const size_t level;
+    unordered_map<Key, PriorityMap*> pmaps;
+    vector<Element> elements;
+
+public:
+    PriorityMap()
+        : key(""), level(0) {       }
+        
+    PriorityMap(Key _key, size_t _level)
+        : key(_key), level(_level) {       }
+
+    ~PriorityMap();
+
+    void push(vector<Key> &label, Element elmt);
+    
+    bool contains(vector<Key> &label) const;
+    size_t size() const;
+    void clear();
+
+
+    class Iterator {
+    private:
+        friend class PriorityMap;
+        PriorityMap* owner;
+        typename vector<Element>::iterator elmt_itr;
+        typename unordered_map<Key, PriorityMap*>::iterator pmap_itr;
+        PriorityMap::Iterator* next_itr;
+        bool base;
+
+
+    public:
+        Iterator(const Iterator& iterator);
+        Iterator()
+            : owner(nullptr), next_itr(nullptr) {       }
+
+        ~Iterator();
+
+        Element& operator*() const;
+
+        Iterator& operator++();
+
+        Iterator& operator=(const Iterator rhs);
+
+        bool operator==(Iterator rhs) const;
+
+        bool operator!=(Iterator rhs) const;
+
+    private:
+        Iterator(PriorityMap<Key, Element>* _owner, typename vector<Element>::iterator elmt, 
+            typename unordered_map<Key, PriorityMap*>::iterator pmap);
+
+        bool done();
+    };
+    
+    
+    Iterator begin();
+    Iterator end();
+    Iterator begin(vector<Key> &key);
+    Iterator end(vector<Key> &key);
+};
+
+/*
+
+    PRIORITYMAP
+
+*/
+
+template <typename Key, typename Element>
+PriorityMap<Key, Element>::~PriorityMap<Key, Element>() {
+    for (const auto& [key, pmap] : pmaps)
+        delete pmap;
+}
+
+template <typename Key, typename Element>
+void PriorityMap<Key, Element>::push(vector<Key> &label, Element elmt) {
+    if (label.empty()) {
+        return;
+    } else if (label.size() == level) { // this is pmap to push elmt
+        elements.push_back(elmt);
+    } else if (pmaps.count(label[level])) { // next pmap exists
+        pmaps[label[level]]->push(label, elmt);
+    } else { // create new pmap
+        pmaps[label[level]] = new PriorityMap(label[level], level + 1);
+        pmaps[label[level]]->push(label, elmt);
+    }
+}
+
+template <typename Key, typename Element>
+bool PriorityMap<Key, Element>::contains(vector<Key> &label) const {
+    if (label.empty())
+        return false;
+    else if (level < label.size() && pmaps.count(label[level]))
+        return pmaps.at(label[level])->contains(label);
+    else 
+        return level == label.size();
+}
+
+template <typename Key, typename Element>
+size_t PriorityMap<Key, Element>::size() const {
+    size_t total = elements.size();
+    for (const auto& [key, pmap] : pmaps)
+        total += pmap->size();
+    return total;
+}
+
+template <typename Key, typename Element>
+void PriorityMap<Key, Element>::clear() {
+    elements.clear();
+    for (const auto& [key, pmap] : pmaps)
+        delete pmap;
+    pmaps.clear();
+}
+
+template <typename Key, typename Element>
+typename PriorityMap<Key, Element>::Iterator PriorityMap<Key, Element>::begin() {
+    return Iterator(this, elements.begin(), pmaps.begin());
+}
+
+template <typename Key, typename Element>
+typename PriorityMap<Key, Element>::Iterator PriorityMap<Key, Element>::end() {
+    return Iterator(this, elements.end(), pmaps.end());
+}
+
+template <typename Key, typename Element>
+typename PriorityMap<Key, Element>::Iterator PriorityMap<Key, Element>::begin(vector<Key> &label) {
+    if (label.empty())
+        return end();
+    else if (level == label.size()) // last label
+        return Iterator(this, elements.begin(), pmaps.begin());
+    else if (level < label.size() && pmaps.count(label[level])) // not last label
+        return pmaps.at(label[level])->begin(label);
+    else
+        return end();
+}
+
+template <typename Key, typename Element>
+typename PriorityMap<Key, Element>::Iterator PriorityMap<Key, Element>::end(vector<Key> &label) {
+    if (label.empty())
+        return end();
+    else if (level == label.size()) // last label
+        return Iterator(this, elements.end(), pmaps.end());
+    else if (level < label.size() && pmaps.count(label[level])) // not last label
+        return pmaps.at(label[level])->end(label);
+    else
+        return end();
+}
+
+
+/*
+
+    ITERATOR
+
+*/
+
+template <typename Key, typename Element>
+PriorityMap<Key, Element>::Iterator::Iterator(const Iterator& iterator) {
+    owner = iterator.owner;
+    elmt_itr = iterator.elmt_itr;
+    pmap_itr = iterator.pmap_itr;
+    if (iterator.next_itr)
+        next_itr = new Iterator(iterator.next_itr->owner, iterator.next_itr->elmt_itr, iterator.next_itr->pmap_itr);
+    else
+        next_itr = nullptr;
+}
+
+template <typename Key, typename Element>
+PriorityMap<Key, Element>::Iterator::Iterator(PriorityMap<Key, Element>* _owner, typename vector<Element>::iterator elmt, 
+            typename unordered_map<Key, PriorityMap*>::iterator pmap)
+    : owner(_owner), elmt_itr(elmt), pmap_itr(pmap), next_itr(nullptr), base(true) {
+    if (elmt_itr == owner->elements.end())
+        ++(*this);
+}
+
+template <typename Key, typename Element>
+PriorityMap<Key, Element>::Iterator::~Iterator() {
+    if (next_itr) {
+        delete next_itr;
+        next_itr = nullptr;
+    }
+}
+
+template <typename Key, typename Element>
+Element& PriorityMap<Key, Element>::Iterator::operator*() const {
+    return next_itr ? *(*next_itr) : *elmt_itr;
+}
+
+template <typename Key, typename Element>
+typename PriorityMap<Key, Element>::Iterator& PriorityMap<Key, Element>::Iterator::operator=(const Iterator rhs) {
+    owner = rhs.owner;
+    elmt_itr = rhs.elmt_itr;
+    pmap_itr = rhs.pmap_itr;
+    if (next_itr)
+        delete next_itr;
+
+    if (rhs.next_itr)
+        next_itr = new Iterator(rhs.next_itr->owner, rhs.next_itr->elmt_itr, rhs.next_itr->pmap_itr);
+    else
+        next_itr = nullptr;
+
+    return *this;
+}
+
+template <typename Key, typename Element>
+typename PriorityMap<Key, Element>::Iterator& PriorityMap<Key, Element>::Iterator::operator++() {
+    if (elmt_itr != owner->elements.end()) { // more base elements
+        elmt_itr++;
+
+        if (elmt_itr == owner->elements.end()) // next_itr is nullptr
+            ++(*this); // moves next_itr to pmap_itr if able
+    } else if (next_itr) { // more elements in next pmap
+        ++(*next_itr);
+        if (next_itr->done()) {
+            delete next_itr;
+            next_itr = nullptr;
+            ++(*this);
+        }
+    } else if (pmap_itr != owner->pmaps.end()) { // more pmaps
+        if (base)
+            base = false;
+        else
+            ++pmap_itr;
+
+        if (pmap_itr != owner->pmaps.end()) { // more elements in pmaps
+            auto next_pmap = pmap_itr->second;
+            auto next_elmt_itr = pmap_itr->second->elements.begin();
+            auto next_pmap_itr = pmap_itr->second->pmaps.begin();
+            next_itr = new Iterator(next_pmap, next_elmt_itr, next_pmap_itr);
+        }
+    }
+
+    return *this;
+}
+
+template <typename Key, typename Element>
+bool PriorityMap<Key, Element>::Iterator::operator==(Iterator rhs) const {
+    bool equal;
+    if (next_itr && rhs.next_itr) {
+        equal = *next_itr == *(rhs.next_itr);
+    } else if (!next_itr && !rhs.next_itr) {
+        equal = true;
+    } else {
+        equal = false;
+    }
+    return elmt_itr == rhs.elmt_itr && pmap_itr == rhs.pmap_itr && equal;
+}
+
+template <typename Key, typename Element>
+bool PriorityMap<Key, Element>::Iterator::operator!=(Iterator rhs) const {
+    return elmt_itr != rhs.elmt_itr || pmap_itr != rhs.pmap_itr || next_itr != rhs.next_itr;
+}
+
+template <typename Key, typename Element>
+bool PriorityMap<Key, Element>::Iterator::done() {
+    bool next_done = next_itr ? next_itr->done() : true;
+    return elmt_itr == owner->elements.end() && pmap_itr == owner->pmaps.end() && next_done;
+}
+
+#endif
---
+++ test/test.cpp
@@ -0,0 +1,460 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include "PriorityMap.h"
+
+using namespace std;
+
+class Instance {
+public:
+    vector<string> label;
+    string element;
+};
+
+string label1 = "one";
+string label2 = "two";
+string label3 = "three";
+vector<Instance> test = {
+    {{label1, label2, label3}, "1 2 3"}, // 0
+    {{label1, label3, label2}, "1 3 2"}, // 1
+    {{label3, label2, label1}, "3 2 1"}, // 2
+    {{label3, label1, label2}, "3 1 2"}, // 3
+    {{label2, label3, label1}, "2 3 1"}, // 4
+    {{label2, label1, label3}, "2 1 3"}, // 5
+    {{label1, label2}, "1 2"}, // 6
+    {{label1, label3}, "1 3"}, // 7
+    {{label2, label1}, "2 1"}, // 8
+    {{label2, label3}, "2 3"}, // 9
+    {{label3, label1}, "3 1"}, // 10
+    {{label3, label2}, "3 2"}, // 11
+    {{label1}, "1"},  // 12
+    {{label2}, "2"}, // 13
+    {{label3}, "3"}, // 14
+    {{label1, label1}, "1 1"}, // 15
+    {{label2, label2}, "2 2"}, // 16
+    {{label3, label3}, "3 3"}, // 17
+    {{label1, label2, label3}, "1 2 1"}, // 18
+    {{label1, label2, label3}, "1 2 2"}, // 19
+};
+
+void print(bool verbose, string message) {
+    if (verbose) {
+        cout << message << endl;
+    }
+}
+void print(bool verbose, string message, int expected, int actual) {
+    if (expected != actual) {
+        cout << "FAIL ";
+        cout << message << endl;
+        cout << "\tExpected: " << expected << endl;
+        cout << "\tActual:   " << actual << endl;
+    } else if (verbose) {
+        cout << "SUCCESS: ";
+        cout << message << endl;
+        cout << "\tExpected: " << expected << endl;
+        cout << "\tActual:   " << actual << endl;
+    }
+}
+void print(bool verbose, string message, string expected, string actual) {
+    if (expected != actual) {
+        cout << "FAIL ";
+        cout << message << endl;
+        cout << "\tExpected: " << expected << endl;
+        cout << "\tActual:   " << actual << endl;
+    } else if (verbose) {
+        cout << "SUCCESS: ";
+        cout << message << endl;
+        cout << "\tExpected: " << expected << endl;
+        cout << "\tActual:   " << actual << endl;
+    }
+}
+void print(bool verbose, string message, bool expected, bool actual) {
+    if (expected != actual) {
+        cout << "FAIL ";
+        cout << message << endl;
+        cout << "\tExpected: " << expected << endl;
+        cout << "\tActual:   " << actual << endl;
+    } else if (verbose) {
+        cout << "SUCCESS: ";
+        cout << message << endl;
+        cout << "\tExpected: " << expected << endl;
+        cout << "\tActual:   " << actual << endl;
+    }
+}
+
+void push(bool v) {
+    vector<Instance> push_test = {
+        {{"1", "2", "3"}, "dup"}, // same
+        {{"1", "2", "3"}, "dup"}, // same
+        {{"1", "2", "5"}, "different label"},
+        {{"1", "2", "3"}, "different elmt"},
+        {{"1", "2", "3"}, "a"},
+    };
+
+    Instance empty_label = {{}, "empty"};
+    Instance empty_elmt = {{"not", "empty"}, ""};
+    Instance empty_instance = {{}, ""};
+
+    PriorityMap<string, string> pmap;
+    
+    pmap.push(push_test[0].label, push_test[0].element);
+    pmap.push(push_test[0].label, push_test[0].element);
+    print(v, "1: push(): duplicate pushes", 2, pmap.size());
+    pmap.clear();
+    
+    pmap.push(push_test[2].label, push_test[2].element);
+    pmap.push(push_test[3].label, push_test[3].element);
+    print(v, "2: push(): different pushes", 2, pmap.size());
+    pmap.clear();
+    
+    pmap.push(empty_label.label, empty_label.element);
+    print(v, "3: push(): empty label", 0, pmap.size());
+    pmap.clear();
+    
+    pmap.push(empty_elmt.label, empty_elmt.element);
+    print(v, "4: push(): empty element", 1, pmap.size());
+    pmap.clear();
+
+    pmap.push(empty_instance.label, empty_instance.element);
+    print(v, "5: push(): empty instance", 0, pmap.size());
+    pmap.clear();
+}
+
+void clear(bool v) {
+    vector<Instance> clear_test = {
+        {{"1", "2", "3"}, "dup"}, 
+        {{"1", "2", "3"}, "dup"}, 
+        {{"1", "2", "5"}, "different label"},
+        {{"1", "2", "3"}, "different elmt"},
+        {{"as", "2", "3"}, "a"}, // 4
+    };
+
+    Instance empty_label = {{}, "empty"};
+    Instance empty_elmt = {{"not", "empty"}, ""};
+    Instance empty_instance = {{}, ""};
+
+    PriorityMap<string, string> pmap;
+    
+    pmap.push(clear_test[0].label, clear_test[0].element);
+    pmap.push(clear_test[0].label, clear_test[0].element);
+    pmap.clear();
+    print(v, "1: clear(): duplicate pushes", 0, pmap.size());
+    
+    pmap.push(clear_test[0].label, clear_test[0].element);
+    pmap.push(clear_test[0].label, clear_test[0].element);
+    pmap.push(clear_test[4].label, clear_test[4].element);
+    pmap.clear();
+    pmap.clear();
+    print(v, "2: clear(): double clear", 0, pmap.size());
+
+    
+    pmap.clear();
+    pmap.push(clear_test[0].label, clear_test[0].element);
+    print(v, "3: clear(): clear before push", 1, pmap.size());
+    pmap.clear();
+
+    
+    pmap.clear();
+    print(v, "1: clear(): empty clear", 0, pmap.size());
+}
+
+void contains(bool v) {
+    PriorityMap<string, string> pmap;
+    
+    pmap.push(test[0].label, test[0].element);
+    print(v, "1: contains(): same label", true, pmap.contains(test[0].label));
+    pmap.clear();
+    
+    pmap.push(test[0].label, test[0].element);
+    print(v, "2: contains(): smaller label", true, pmap.contains(test[6].label));
+    pmap.clear();
+
+    pmap.push(test[6].label, test[6].element);
+    print(v, "3: contains(): bigger label", false, pmap.contains(test[0].label));
+    pmap.clear();
+
+    pmap.push(test[0].label, test[0].element);
+    print(v, "4: contains(): mixed label", false, pmap.contains(test[1].label));
+    pmap.clear();
+
+    pmap.push(test[12].label, test[12].element);
+    print(v, "5: contains(): duplicate label", false, pmap.contains(test[15].label));
+    pmap.clear();
+
+    pmap.push(test[6].label, test[6].element);
+    print(v, "6: contains(): reversed label", false, pmap.contains(test[8].label));
+    pmap.clear();
+
+    pmap.push(test[17].label, test[17].element);
+    print(v, "7: contains(): wrong earlier label", false, pmap.contains(test[7].label));
+    pmap.clear();
+
+    pmap.push(test[8].label, test[8].element);
+    print(v, "8: contains(): wrong earlier label different size", false, pmap.contains(test[12].label));
+    pmap.clear();
+    
+    pmap.push(test[0].label, test[0].element);
+    pmap.push(test[6].label, test[6].element);
+    print(v, "9: contains(): same label after shorter push", true, pmap.contains(test[0].label));
+    pmap.clear();
+}
+
+void iterator_memory(bool v) {
+    PriorityMap<string, string> pmap;
+    
+    auto itr1 = pmap.begin();
+    print(v, "1: iterator_memory(): not saving begin()", true, pmap.begin() == pmap.begin());
+    
+    itr1 = pmap.begin();
+    print(v, "SUCCESS: 2. iterator_memory(): itr reassigned");
+
+    pmap.push(test[3].label, test[3].element);
+    pmap.push(test[10].label, test[10].element);
+    pmap.push(test[12].label, test[12].element);
+    pmap.push(test[0].label, test[0].element);
+    print(v, "SUCCESS: 3. iterator_memory(): push after itr reassigned");
+    itr1 = pmap.begin();
+    print(v, "SUCCESS: 4. iterator_memory(): itr reassigned after push");
+
+    auto itr2 = itr1;
+    print(v, "SUCCESS: 5. iterator_memory(): assigning itr to itr");
+
+
+    pmap.clear();
+    print(v, "SUCCESS: 6. iterator_memory(): clear");
+
+    pmap.push(test[12].label, test[12].element);
+    ++itr1;
+    print(v, "SUCCESS: 7. iterator_memory(): itr++");
+
+    itr2 = itr1;
+    print(v, "SUCCESS: 8. iterator_memory(): itr = itr after operation++");
+
+    ++itr1;
+    print(v, "SUCCESS: 9. iterator_memory():  operation++ after itr = itr");
+
+    print(v, "10: iterator_memory(): itr != ++itr", true, itr1 != itr2);
+}
+
+void iterator_functs(bool v) {
+    PriorityMap<string, string> pmap;
+    
+    auto itr = pmap.begin();
+    print(v, "1: iterator_functs(): itr == itr", true, itr == itr);
+    print(v, "2: iterator_functs(): itr != itr", false, itr != itr);
+    print(v, "3: iterator_functs(): begin() == begin()", true, pmap.begin() == pmap.begin());
+    print(v, "4: iterator_functs(): begin() != begin()", false, pmap.begin() != pmap.begin());
+    print(v, "5: iterator_functs(): end() == end()", true, pmap.end() == pmap.end());
+    print(v, "6: iterator_functs(): end() != end()", false, pmap.end() != pmap.end());
+    print(v, "7: iterator_functs(): empty, begin() == end()", true, pmap.begin() == pmap.end());
+    print(v, "8: iterator_functs(): empty, end() == begin()", true, pmap.end() == pmap.begin());
+    print(v, "9: iterator_functs(): empty, begin() != end()", false, pmap.begin() != pmap.end());
+    print(v, "10: iterator_functs(): empty, end() != begin()", false, pmap.end() != pmap.begin());
+
+
+
+    pmap.push(test[0].label, test[0].element);
+    itr = pmap.begin();
+    print(v, "11: iterator_functs(): one element, begin() == end()", false, pmap.begin() == pmap.end());
+    print(v, "12: iterator_functs(): one element, itr == end()", false, itr == pmap.end());
+    print(v, "13: iterator_functs(): one element, itr == begin()", true, itr == pmap.begin());
+    print(v, "14: iterator_functs(): one element, itr == itr", true, itr == itr);
+
+    print(v, "15: iterator_functs(): element == *itr", test[0].element, *itr);
+}
+
+void iterator_loops(bool v) {
+    PriorityMap<string, string> pmap;
+
+    int actual = 0;
+    int expected = 1;
+
+    pmap.push(test[12].label, test[12].element);
+    auto itr = pmap.begin();
+    auto end = pmap.end();
+    ++itr;
+    bool equal = itr == end;
+    print(v, "1: iterator_loops(): ++itr == end", true, equal);
+    
+    actual = 0;
+    expected = 1;
+    for (auto it = pmap.begin(); it != pmap.end(); ++it) {
+        ++actual;
+    }
+    print(v, "2: iterator_loops(): one elmt for loop", expected, actual);
+    pmap.clear();
+
+    pmap.push(test[12].label, test[12].element);
+    pmap.push(test[12].label, test[12].element);
+    pmap.push(test[3].label, test[3].element);
+    pmap.push(test[10].label, test[10].element);
+    pmap.push(test[3].label, test[3].element);
+    actual = 0;
+    expected = 5;
+    for (auto it = pmap.begin(); it != pmap.end(); ++it) {
+        ++actual;
+    }
+    print(v, "3: iterator_loops(): five elmt for loop", expected, actual);
+    pmap.clear();
+
+    
+
+    pmap.push(test[12].label, test[12].element);
+    pmap.push(test[18].label, test[18].element);
+    pmap.push(test[3].label, test[3].element);
+    pmap.push(test[10].label, test[10].element);
+    pmap.push(test[3].label, test[3].element);
+    pmap.push(test[13].label, test[13].element);
+    pmap.push(test[13].label, test[13].element);
+    actual = 0;
+    expected = 7;
+    for (auto it = pmap.begin(); it != pmap.end(); ++it) {
+        ++actual;
+    }
+    print(v, "4: iterator_loops(): 7 elmt for loop", expected, actual);
+    pmap.clear();
+
+    
+
+    vector<int> actual_v(test.size(), 1); // going to push one duplicate
+    size_t dup_index = 9;
+    ++actual_v[dup_index];
+    for (size_t i = 0; i < test.size(); ++i) {
+        pmap.push(test[i].label, test[i].element);
+    }
+    pmap.push(test[dup_index].label, test[dup_index].element);   
+    for (auto it = pmap.begin(); it != pmap.end(); ++it) {
+        for (size_t i = 0; i < test.size(); ++i) {
+            if (*it == test[i].element) {
+                --actual_v[i];
+            }
+        }
+    }
+    bool good = true;
+    for (size_t i = 0; i < actual_v.size(); ++i) {
+        if (actual_v[i] != 0) {
+            good = false;
+            break;
+        }
+    }
+    print(v, "5: iterator_loops(): expected counts by operator*", true, good);
+    pmap.clear();
+
+
+    vector<int> actual_v2(test.size(), 0); // going to push one duplicate
+    for (size_t i = 0; i < test.size(); ++i) {
+        for (size_t j = 0; j < i % 3; ++j) {
+            pmap.push(test[i].label, test[i].element);
+            ++actual_v[i];
+        }
+    }  
+    for (auto it = pmap.begin(); it != pmap.end(); ++it) {
+        for (size_t i = 0; i < test.size(); ++i) {
+            if (*it == test[i].element) {
+                --actual_v[i];
+            }
+        }
+    }
+    good = true;
+    for (size_t i = 0; i < actual_v.size(); ++i) {
+        if (actual_v[i] != 0) {
+            good = false;
+            break;
+        }
+    }
+    print(v, "6: iterator_loops(): random(ish) expected counts by operator*", true, good);
+    pmap.clear();
+
+    
+    // label1
+    vector<string> vlabel1 = {label1};
+    pmap.push(test[12].label, test[12].element);
+    pmap.push(test[1].label, test[1].element);
+    pmap.push(test[14].label, test[14].element);
+    pmap.push(test[0].label, test[0].element);
+    pmap.push(test[9].label, test[9].element);
+    pmap.push(test[1].label, test[1].element);
+    pmap.push(test[6].label, test[6].element);
+    actual = 0;
+    expected = 5;
+    for (auto it = pmap.begin(vlabel1); it != pmap.end(vlabel1); ++it) {
+        ++actual;
+    }
+    print(v, "7: iterator_loops(): begin(vector)", expected, actual);
+    pmap.clear();
+
+    
+    vector<string> vlabel2 = {label3};
+    pmap.push(test[12].label, test[12].element); // 1
+    pmap.push(test[1].label, test[1].element); // 1 3 2
+    pmap.push(test[0].label, test[0].element); // 1 2 3
+    pmap.push(test[9].label, test[9].element); // 2 3
+    pmap.push(test[1].label, test[1].element); // 1 3 2
+    pmap.push(test[6].label, test[6].element); // 1 2
+    actual = 0;
+    expected = 0;
+    for (auto it = pmap.begin(vlabel2); it != pmap.end(vlabel2); ++it) {
+        ++actual;
+    }
+    print(v, "8: iterator_loops(): begin(vector) no actual", expected, actual);
+    pmap.clear();
+
+    
+    vector<string> vlabel3 = {label1, label2};
+    pmap.push(test[12].label, test[12].element); // 1
+    pmap.push(test[1].label, test[1].element); // 1 3 2
+    pmap.push(test[0].label, test[0].element); // 1 2 3
+    pmap.push(test[9].label, test[9].element); // 2 3
+    pmap.push(test[1].label, test[1].element); // 1 3 2
+    pmap.push(test[6].label, test[6].element); // 1 2
+    pmap.push(test[10].label, test[10].element); // 3 1
+    pmap.push(test[8].label, test[8].element); // 2 1
+    pmap.push(test[18].label, test[18].element); // 1 2 1
+    actual = 0;
+    expected = 3;
+    for (auto it = pmap.begin(vlabel3); it != pmap.end(vlabel3); ++it) {
+        ++actual;
+    }
+    print(v, "9: iterator_loops(): begin(vector), vector.size() == 2", expected, actual);
+    pmap.clear();
+}
+
+void iterator_assignment(bool v) {
+    PriorityMap<string, string> pmap;
+
+    pmap.push(test[12].label, test[12].element);
+    pmap.push(test[12].label, test[12].element);
+    pmap.push(test[3].label, test[3].element);
+    pmap.push(test[10].label, test[10].element);
+    pmap.push(test[3].label, test[3].element);
+    int actual = 0;
+    int expected = 3;
+    auto itr = pmap.begin();
+    for (size_t i = 0; i < 2; ++i)
+        ++itr;
+    auto itr2 = itr;
+    bool same = true;
+    for (; itr2 != pmap.end() && itr != pmap.end(); ++itr, ++itr2) {
+        if (*itr != *itr2)
+            same = false;
+        ++actual;
+    }
+    print(v, "1: iterator_assignment(): same itr amount", expected, actual);
+    print(v, "2: iterator_assignment(): same itr element", true, same);
+    pmap.clear();
+}
+
+
+int main(int argc, char** argv) {
+    bool verbose = false;
+    if (argc == 2 && (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--verbose") == 0)) {
+        verbose = true;
+    }
+
+    clear(verbose);
+    contains(verbose);
+    iterator_memory(verbose);
+    iterator_functs(verbose);
+    iterator_loops(verbose);
+    return 0;
+}
